983c56b0a8df7657e736f89d1882712960d31b4e
FireRedz <fireredz69420@gmail.com>
2022-03-07 10:48:40 +0800

    Sane settings

--- game/settings/settings.v
+++ game/settings/settings.v
@@ -12,10 +12,16 @@ pub struct Window {
 		fps    f64 // Update tick
 		record bool
 		record_fps f64 // Record tick 
+
+		// Volumes
+		audio_volume f64
+		effect_volume f64
+		overall_volume f64
 }
 
 pub struct Gameplay {
 	pub mut:
+		global_offset  f64
 		lead_in_time   f64
 		background_dim int
 
@@ -37,10 +43,13 @@ pub struct Gameplay {
 // Factory
 pub fn make_window_settings() &Window {
 	mut window_ := &Window{
-		speed: 1.0,
+		speed: 1.5,
 		fps: 60,
 		record: false,
 		record_fps: 60
+		audio_volume: 50,
+		effect_volume: 100,
+		overall_volume: 50,
 	}
 
 	return window_
@@ -48,20 +57,21 @@ pub fn make_window_settings() &Window {
 
 pub fn make_gameplay_settings() &Gameplay {
 	mut gameplay_ := &Gameplay{
+		global_offset: 0,
 		lead_in_time: 3.0 * 1000.0, // n Seconds
-		background_dim: 50,
+		background_dim: 100,
 
-		disable_hitsound: true,
+		disable_hitsound: false,
 		disable_hitobject: false,
 		disable_storyboard: false,
 
-		use_beatmap_hitsound: false,
+		use_beatmap_hitsound: true,
 
 		disable_cursor: false,
 		cursor_size: 0.75,
 		cursor_trail_update_rate: 16.6667, // 60FPS delta
 		cursor_trail_length: 1000, // Maximum length
-		cursor_style: 0, // 0: Normal, 1: Particle (terrible), 2: Long (Like style 0 but the trail is smoother)
+		cursor_style: 2, // 0: Normal, 1: Particle (terrible), 2: Long (Like style 0 but the trail is smoother)
 
 		auto_update_rate: 16.6667
 	}
