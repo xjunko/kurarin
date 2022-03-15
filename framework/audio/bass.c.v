d3f715a0bc599c8c50047e4718b73579fa8e7cf6
FireRedz <fireredz69420@gmail.com>
2022-03-15 10:31:56 +0800

    Add BASS_ChannelGetData

--- framework/audio/bass.c.v
+++ framework/audio/bass.c.v
@@ -16,6 +16,9 @@ fn C.BASS_ChannelSetAttribute(C.HSTREAM, int, f32)
 fn C.BASS_ChannelSetPosition(C.HSTREAM, C.QWORD, int)
 
 // Utils
-fn C.BASS_SampleGetChannel(C.HSTREAM, int) C.HSTREAM
+fn C.BASS_SampleGetChannel(C.HSAMPLE, int) C.HSTREAM
 fn C.BASS_SampleLoad(int, &char, int, int, int, int) C.HSTREAM
 fn C.BASS_ChannelSeconds2Bytes(C.HSTREAM, f64) C.QWORD
+
+// FFT
+fn C.BASS_ChannelGetData(C.HSTREAM, &voidptr, int)
