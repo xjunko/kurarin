e63d222c213bba5f72dc6fbdabdf322d857ac83a
FireRedz <fireredz69420@gmail.com>
2022-03-18 14:44:31 +0800

    Support more storyboard easing

--- framework/math/easing/enums.v
+++ framework/math/easing/enums.v
@@ -40,11 +40,32 @@ pub enum Easing {
 }
 
 pub fn get_easing_from_enum(e Easing) EasingFunction {
-	return match e {
-		.linear { linear }
-		.quad_in { quad_in }
-		.quad_out { quad_out }
-		.quad_in_out { quad_in_out }
+	// return match e {
+	// 	.linear { linear }
+	// 	.quad_in { quad_in }
+	// 	.quad_out { quad_out }
+	// 	.quad_in_out { quad_in_out }
+	// 	else { quad_out }
+	// }
+
+	// This is fucked
+	return match int(e) {
+		int(Easing.linear) { linear }
+		int(Easing.quad_in) { quad_in }
+		int(Easing.quad_out) { quad_out }
+		int(Easing.quad_in_out) { quad_in_out }
+		int(Easing.quart_in) { quart_in }
+		int(Easing.quart_out) { quart_out }
+		int(Easing.quart_in_out) { quart_in_out }
+		int(Easing.quint_in) { quint_in }
+		int(Easing.quint_out) { quint_out }
+		int(Easing.quint_in_out) { quint_in_out }
+		int(Easing.sine_in) { sine_in }
+		int(Easing.sine_out) { sine_out }
+		int(Easing.sine_in_out) { sine_in_out }
+		int(Easing.elastic_in) { elastic_in }
+		int(Easing.elastic_out) { elastic_out }
+		int(Easing.elastic_in_out) { elastic_in_out }
 		else { quad_out }
 	}
 
@@ -54,15 +75,15 @@ pub fn get_easing_from_enum(e Easing) EasingFunction {
 	// 	.quad_in { quad_in }
 	// 	.quad_out { quad_out }
 	// 	.quad_in_out { quad_in_out }
-	// 	.quart_in { quart_in }
-	// 	.quart_out { quart_out }
-	// 	.quart_in_out { quart_in_out }
-	// 	.quint_in { quint_in }
-	// 	.quint_out { quint_out }
-	// 	.quint_in_out { quint_in_out }
-	// 	.sine_in { sine_in }
-	// 	.sine_out { sine_out }
-	// 	.sine_in_out { sine_in_out }
+	// 	// .quart_in { quart_in }
+	// 	// .quart_out { quart_out }
+	// 	// .quart_in_out { quart_in_out }
+	// 	// .quint_in { quint_in }
+	// 	// .quint_out { quint_out }
+	// 	// .quint_in_out { quint_in_out }
+	// 	// .sine_in { sine_in }
+	// 	// .sine_out { sine_out }
+	// 	// .sine_in_out { sine_in_out }
 	// 	else { quad_out }
 	// }
 }
