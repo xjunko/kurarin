736f9e8f12c5c2cc8539dec18e1c0e6ca2663499
FireRedz <fireredz69420@gmail.com>
2022-03-18 14:44:21 +0800

    Add elastic (in/out/in_out) ease function

--- framework/math/easing/easing.v
+++ framework/math/easing/easing.v
@@ -113,3 +113,80 @@ pub fn sine_in_out(t f64, b f64, c f64, d f64) f64 {
 	return -c / 2 * (math.cos(math.pi * t / d) - 1) + b
 }
 
+pub fn elastic_in(_t f64, b f64, c f64, d f64) f64 {
+	mut s := 1.70158
+	mut a := c
+	mut t := _t
+
+	if t == 0 {
+		return b
+	}
+
+	t /= d
+
+	if t == 1 {
+		return b + c
+	}
+
+	p := d * 0.3
+	if a < math.abs(c) {
+		a = c
+		s = p / 4
+	} else {
+		s = p / (2 * math.pi) * math.asin(c / a)
+	}
+
+	t -= 1
+	return -(a * math.pow(2, 10 * t) * math.sin((t * d - s) * (2 * math.pi) / p)) + b
+}
+
+pub fn elastic_out(_t f64, b f64, c f64, d f64) f64 {
+	mut s, mut a := 1.70158, c
+	mut t := _t
+
+	if t == 0 {
+		return b
+	}
+	t /= d
+	if t == 1 {
+		return b + c
+	}
+
+	mut p := d * 0.3
+	if a < math.abs(c) {
+		a, s = c, p / 4
+	} else {
+		s = p / (2 * math.pi) * math.asin(c / a)
+	}
+
+	return a * math.pow(2, -10 * t) * math.sin((t * d - s) * (2 * math.pi) / p) + c + b
+}
+
+pub fn elastic_in_out(_t f64, b f64, c f64, d f64) f64 {
+	mut s, mut a := 1.70158, c
+	mut t := _t
+
+	if t == 0 {
+		return b
+	}
+		
+	t /= (d / 2)
+
+	if t == 2 {
+		return b + c
+	}
+
+	p := d * (0.3 * 1.5)
+	if a < math.abs(c) {
+		a, s = c, p / 4
+	} else {
+		s = p / (2 * math.pi) * math.asin(c / a)
+	}
+		
+	if t < 1 {
+		t -= 1
+		return -0.5 * (a * math.pow(2, 10 * t) * math.sin((t * d - s) * (2 * math.pi) / p)) + b
+	}
+
+	return 0.0
+}
