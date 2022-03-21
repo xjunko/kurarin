cf4cbc3ac91786a20807092862dbb86aa56a6ff6
FireRedz <fireredz69420@gmail.com>
2022-03-21 23:32:35 +0800

    "Proper" build script

--- ext/build.sh
+++ ext/build.sh
@@ -1,1 +1,29 @@
-clear && v shader assets/shaders/slider.glsl && v -cc gcc -gc boehm .
+build_shaders() {
+    v shader assets/shaders/slider.glsl
+}
+
+build_development() {
+    clear &&
+    echo "Build: Development" &&
+    build_shaders &&
+    v -cc gcc -gc boehm .
+}
+
+build_production() {
+    clear &&
+    echo "Build: Production" &&
+    build_shaders &&
+    v -cc gcc -gc boehm -prod .
+}
+
+case $1 in 
+    "--prod"|"--production"|"-prod")
+        build_production
+        exit 0
+        ;;
+    *)
+    
+    # devel build?
+    build_development
+
+esac
