c73baa6383f838f17d7b4888759a57a1502bf28f
FireRedz <fireredz69420@gmail.com>
2022-04-08 23:16:49 +0800

    Unused McOsu resolution calc

--- game/x/x.v
+++ game/x/x.v
@@ -27,7 +27,8 @@ pub fn (mut res Resolution) calculate() {
 	// res.playfield_scale *= res.scale * 2
 	// res.offset.x = (res.resolution.x - res.playfield.x) / 2.1
 	// res.offset.y = (res.resolution.y - res.playfield.y) / 1.9
-	
+
+	// osr2mp4
 	res.playfield.y = res.resolution.y * 0.8
 	res.playfield.x = res.playfield.y * 4/3
 	res.playfield_scale = res.playfield.x / 512
@@ -35,8 +36,42 @@ pub fn (mut res Resolution) calculate() {
 	res.offset.x = (res.resolution.x - res.playfield.x) / 2.0
 	res.offset.y = (res.resolution.y * 0.1)
 
-	logging.debug(res.str())
+	// mcosu
+	// res.playfield = get_playfield_size(res)
+	// res.playfield_scale = get_playfield_size_factor(res)
+	// res.offset = get_playfield_offset(res)
+	// res.scale = res.resolution.y / 768
+
+	logging.info(res.str())
 	logging.debug("Resolution calculated!")
+}
+
+
+// from mcosu
+pub fn get_playfield_size_factor(res Resolution) f64 {
+	screen_width := res.resolution.x
+	top_border_size := 0.117 * res.resolution.y
+	bottom_border_size := 0.0834 * res.resolution.y
+	screen_height := res.resolution.y - bottom_border_size - top_border_size
+
+	if res.resolution.x / 512.0 > screen_height / 384.0 {
+		return screen_height / 384.0
+	} else {
+		return screen_width / 512.0
+	}
+}
+
+pub fn get_playfield_size(res Resolution) vector.Vector2 {
+	return vector.Vector2{512.0, 384.0}.scale(get_playfield_size_factor(res))
+}
+
+pub fn get_playfield_offset(res Resolution) vector.Vector2 {
+	playfield_size := get_playfield_size(res)
+	bottom_border_size := 0.0834 * res.resolution.y
+	playfield_y_offset := (res.resolution.y / 2.0 - (playfield_size.y / 2.0)) - bottom_border_size
+
+	return vector.Vector2{(res.resolution.x - playfield_size.x) / 2.0, (res.resolution.y - playfield_size.y) / 2.0 + playfield_y_offset}
+	
 }
 
 
