a55cd509da634dda51c099e295a323badb94ba38
FireRedz <fireredz69420@gmail.com>
2022-04-23 21:03:36 +0800

    Add get_texture_with_fallback and get_frames

--- game/skin/skin.v
+++ game/skin/skin.v
@@ -40,16 +40,45 @@ pub struct Skin {
 pub fn get_texture(name string) gg.Image {
 	mut skin := get_skin()
 
-	// skin is not binded somehow
 	if !skin.bind {
-		logging.fatal("Tried to get texture when not binded.")
+		logging.fatal("Graphic context is not binded while getting texture, this is not supposed to happen.")
 	}
 
-	// not in cache
+	return get_texture_with_fallback(name, name)
+}
+
+pub fn get_frames(name string) []gg.Image {
+	mut skin := get_skin()
+
+	// Check for first frame
+	mut frames := []gg.Image{}
+	mut frame_n := 0
+
+	for os.exists(os.join_path(skin.fallback, '${name}-${frame_n}.png')) {
+		frames << get_texture('${name}-${frame_n}')
+		frame_n++
+	}
+
+	return frames
+}
+
+
+pub fn get_texture_with_fallback(name string, fallback string) gg.Image {
+	mut skin := get_skin()
+
+	// Try get from normal name
 	if name !in skin.cache {
 		skin.cache[name] = skin.ctx.create_image(os.join_path(skin.fallback, name + '.png'))
+
+		// Check if failed
+		if skin.cache[name].id == 0 {
+			logging.error("Failed getting ${name} from skin, trying ${fallback}!")
+			
+			// Get from fallback
+			skin.cache[fallback] = skin.ctx.create_image(os.join_path(skin.fallback, fallback + '.png'))
+			skin.cache[name] = skin.cache[fallback]
+		}
 	}
 
 	return skin.cache[name]
 }
-
