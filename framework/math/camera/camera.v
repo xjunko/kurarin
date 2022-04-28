6441a5ffaf4edd40db682ac701f7d2bf74520856
FireRedz <fireredz69420@gmail.com>
2022-04-28 23:10:41 +0800

    Add translate method into camera

--- framework/math/camera/camera.v
+++ framework/math/camera/camera.v
@@ -7,3 +7,7 @@ pub struct Camera {
 		offset vector.Vector2
 		scale  f64 = f64(1.0)
 }
+
+pub fn (camera &Camera) translate(position vector.Vector2) vector.Vector2 {
+	return position.scale(camera.scale).add(camera.offset)
+}
