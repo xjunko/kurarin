e7268d2f0f35ecbd1f0d41be76983547aa677aab
FireRedz <fireredz69420@gmail.com>
2022-05-16 00:59:16 +0800

    Make LinearMover use quad_out

--- game/movers/linearmover.v
+++ game/movers/linearmover.v
@@ -1,5 +1,6 @@
 module movers
 
+import framework.math.easing
 import framework.math.vector
 
 pub struct LinearMover {
@@ -12,4 +13,8 @@ pub fn (linear &LinearMover) get_point_at(time f64) vector.Vector2 {
 		linear.start.x + (linear.end.x - linear.start.x) * t,
 		linear.start.y + (linear.end.y - linear.start.y) * t,
 	}
+}
+
+pub fn (linear &LinearMover) get_multiplier(time f64) f64 {
+	return easing.quad_out(time - linear.time.start, 0, 1.0, linear.time.duration())
 }
