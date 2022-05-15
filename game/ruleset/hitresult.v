92e7b3e1c5ee524e19b2682f05294b03ac094f3a
FireRedz <fireredz69420@gmail.com>
2022-05-15 13:24:04 +0800

    Add get_value on hitresult

--- game/ruleset/hitresult.v
+++ game/ruleset/hitresult.v
@@ -18,3 +18,18 @@ pub enum HitResult {
 	katu_addition
 	geki_addition
 }
+
+pub fn (result HitResult) get_value() i64 {
+	match result {
+		.hit50 { return 50 }
+		.hit100 { return 100 }
+		.hit300 { return 300 }
+		.slider_start, .slider_repeat, .slider_end { return 30 }
+		.slider_point { return 100 }
+		.spinner_bonus { return 1100 }
+		
+		else {}
+	}
+
+	return 0
+}
