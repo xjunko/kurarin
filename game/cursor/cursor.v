fccb549aef7af37f98efd4cbb406ecb56a237caf
FireRedz <fireredz69420@gmail.com>
2022-07-12 18:54:35 +0800

    Refactored and removed cursor style 1

--- game/cursor/cursor.v
+++ game/cursor/cursor.v
@@ -6,7 +6,6 @@ import time
 import library.gg
 import gx
 import math
-import rand
 import sync
 
 import framework.graphic.sprite
@@ -22,14 +21,10 @@ import game.x
 
 const (
 	used_imports = gameobject.used_imports
+	max_valid_style = 2
 
 	osu_cursor_trail_delta = f64(1000.0 / 120.0) // 120FPS
 )
-
-// V updated its rand library and now its returning optionals (?)
-fn random_f64_in_range(min f64, max f64) f64 {
-	return rand.f64_in_range(min, max) or { 0.0 }
-}
 
 pub struct Cursor {
 	sprite.Sprite
@@ -56,7 +51,8 @@ pub struct Cursor {
 
 pub fn (mut cursor Cursor) draw(_ sprite.CommonSpriteArgument) {
 	cursor.mutex.@lock()
-
+	
+	// TODO: This is fucked, fix this
 	if settings.global.gameplay.cursor.style == 2 {
 		for i, trail in cursor.delta_pos {
 			size := cursor.size.scale(
@@ -94,7 +90,7 @@ pub fn (mut cursor Cursor) draw(_ sprite.CommonSpriteArgument) {
 					height: f32(trail.size.y * x.resolution.playfield_scale)
 				},
 				color: trail.color,
-				additive: settings.global.gameplay.cursor.style != 0
+				additive: false
 			})
 		}
 	}
@@ -102,47 +98,44 @@ pub fn (mut cursor Cursor) draw(_ sprite.CommonSpriteArgument) {
 	// Cursor
 	pos := cursor.position.sub(cursor.origin.multiply(x: cursor.size.x, y: cursor.size.y))
 	
+
+	mut cursor_additive := false
+	mut cursor_color := cursor.color
+	mut cursor_img := &cursor.textures[0]
+
+	// Additive Cursor
 	if settings.global.gameplay.cursor.style == 2 {
-		// Fancy cursor
-		cursor.ctx.draw_image_with_config(gg.DrawImageConfig{
-			img: &cursor.textures[2],
-			img_id: cursor.textures[2].id,
-			img_rect: gg.Rect{
-				x: f32(pos.x * x.resolution.playfield_scale + x.resolution.offset.x),
-				y: f32(pos.y * x.resolution.playfield_scale + x.resolution.offset.y),
-				width: f32(cursor.size.x * x.resolution.playfield_scale),
-				height: f32(cursor.size.y * x.resolution.playfield_scale)
-			},
-			additive: true
-		})
-	} else {
-		// Normal boring cursor
-		cursor.ctx.draw_image_with_config(gg.DrawImageConfig{
-			img: &cursor.textures[0],
-			img_id: cursor.textures[0].id,
-			img_rect: gg.Rect{
-				x: f32(pos.x * x.resolution.playfield_scale + x.resolution.offset.x),
-				y: f32(pos.y * x.resolution.playfield_scale + x.resolution.offset.y),
-				width: f32(cursor.size.x * x.resolution.playfield_scale),
-				height: f32(cursor.size.y * x.resolution.playfield_scale)
-			},
-			color: cursor.color,
-		})
+		cursor_additive = true
+		cursor_color = gx.white
+		cursor_img = &cursor.textures[2]
 	}
+
+	cursor.ctx.draw_image_with_config(gg.DrawImageConfig{
+		img: cursor_img,
+		img_id: cursor_img.id,
+		img_rect: gg.Rect{
+			x: f32(pos.x * x.resolution.playfield_scale + x.resolution.offset.x),
+			y: f32(pos.y * x.resolution.playfield_scale + x.resolution.offset.y),
+			width: f32(cursor.size.x * x.resolution.playfield_scale),
+			height: f32(cursor.size.y * x.resolution.playfield_scale)
+		},
+		additive: cursor_additive,
+		color: cursor_color
+	})
 	
 	cursor.mutex.unlock()
 }
 
 pub fn (mut cursor Cursor) update(time f64, _delta f64) {
 	cursor.mutex.@lock()
+	cursor.Sprite.update(time) // Update the main cursor itself
 
 	// Delta
 	delta := time - cursor.last_time
 
 	// Time
-	cursor.Sprite.update(time) // Update the main cursor itself
-
 	cursor.sixty_delta += delta
+
 	// Normal trails
 	if settings.global.gameplay.cursor.style == 0 && cursor.sixty_delta >= osu_cursor_trail_delta {
 		mut trail := &sprite.Sprite{textures: [cursor.textures[1]]}
@@ -154,23 +147,6 @@ pub fn (mut cursor Cursor) update(time f64, _delta f64) {
 		cursor.trails << trail
 
 		cursor.sixty_delta -= osu_cursor_trail_delta
-	} else if settings.global.gameplay.cursor.style == 1 {
-		// God awful particle trail (Old)
-		distance := cursor.position.distance(cursor.last_position)
-		distance_real := cursor.position.sub(cursor.last_position)
-
-		if distance != 0.0 {
-			for _ in 0 .. rand.int_in_range(10, math.max<int>(math.min<int>(int(distance), 40), 20)) or { 0 } {
-				mut trail := &sprite.Sprite{textures: [cursor.textures[1]]}
-				trail.add_transform(typ: .fade, easing: easing.quad_out, time: time2.Time{time, time + random_f64_in_range(100, math.max(distance * 25.0, 150))}, before: [255.0], after: [0.0])
-				trail.add_transform(typ: .move, easing: easing.quad_out, time: time2.Time{time, time + random_f64_in_range(100, math.max(distance * 32.0, 150))}, before: [cursor.position.x, cursor.position.y], after: [cursor.position.x - distance_real.x + random_f64_in_range(-distance, distance), cursor.position.y - distance_real.y + random_f64_in_range(-distance, distance)])
-				trail.add_transform(typ: .scale_factor, time: time2.Time{time, time}, before: [random_f64_in_range(0.05, settings.global.gameplay.cursor.size)])
-				trail.add_transform(typ: .color, time: time2.Time{time, time}, before: [random_f64_in_range(0, 255), random_f64_in_range(0, 255), random_f64_in_range(0, 255)])
-				trail.reset_size_based_on_texture()
-				trail.reset_attributes_based_on_transforms()
-				cursor.trails << trail
-			}
-		}
 	}
 
 	// Smoother cursor trail (wip)
@@ -193,7 +169,7 @@ pub fn (mut cursor Cursor) update(time f64, _delta f64) {
 		}
 	}
 
-
+	// TODO: this is fucked
 	for mut trail in cursor.trails {
 		if time > trail.time.end {
 			cursor.trails.delete(cursor.trails.index(trail))
