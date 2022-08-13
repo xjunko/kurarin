42ea6b240c8fc85d7fe1250715a97af4e7387d6a
FireRedz <fireredz69420@gmail.com>
2022-08-13 15:41:04 +0800

    `Manager` build flags for ProjectSekai

--- framework/graphic/sprite/manager.v
+++ framework/graphic/sprite/manager.v
@@ -1,18 +1,29 @@
 module sprite
 
+import time as timelib
 import framework.math.camera
 
 pub struct Manager {
 	pub mut:
-		queue []&ISprite
+		queue []&Sprite
 		camera camera.Camera
 		dirty bool
 
 		last_time f64
 }
 
-pub fn (mut manager Manager) add(mut sprite ISprite) {
+pub fn (mut manager Manager) add(mut sprite Sprite) {
 	manager.queue << unsafe { &sprite }
+	sprite.id = manager.queue.len
+}
+
+// TODO: This is slow cuz `O(n)`, but only used once per end time so idk
+pub fn (mut manager Manager) find_index_by_id(id int) int {
+	for i, s in manager.queue {
+		if s.id == id { return i }
+	}
+
+	return -1
 }
 
 pub fn (mut manager Manager) update(time f64) {
@@ -21,7 +32,14 @@ pub fn (mut manager Manager) update(time f64) {
 	for mut sprite in manager.queue {
 		// Remove
 		if time >= sprite.time.end && !sprite.always_visible {
-			manager.queue.delete(manager.queue.index(sprite))
+			// start := f64(timelib.ticks())
+			$if project_sekai ? {
+				manager.queue.delete(manager.find_index_by_id(sprite.id))
+			} $else {
+				manager.queue.delete(manager.queue.index(sprite))
+			}
+			
+			// println("Took: ${f64(timelib.ticks()) - start:.10} to finish")
 			continue
 		}
 
