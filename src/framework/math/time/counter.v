bd802f77681fb379922063cf54486488c9dc57fd
xJunko <fireredz69420@gmail.com>
2023-06-07 21:37:28 +0800

    Time: `ResetArgs`

--- src/framework/math/time/counter.v
+++ src/framework/math/time/counter.v
@@ -41,6 +41,7 @@ pub struct TimeCounter {
 mut:
 	start_time f64
 	last_time  f64
+	offset     f64
 pub mut:
 	delta f64
 	time  f64
@@ -58,7 +59,14 @@ pub fn (mut t TimeCounter) stop() {
 	t.stop = true
 }
 
-pub fn (mut t TimeCounter) reset() {
+[args; params]
+pub struct ResetArgs {
+pub:
+	offset f64
+}
+
+pub fn (mut t TimeCounter) reset(arg ResetArgs) {
+	t.offset = arg.offset
 	t.last_time = timelib.ticks()
 	t.start_time = t.last_time
 	t.time = 0
@@ -72,13 +80,13 @@ pub fn (mut t TimeCounter) tick() f64 {
 	// Most likely a recording timer, dont use system time.
 	if t.use_custom_delta {
 		t.delta = t.custom_delta
-		t.time += t.custom_delta * t.speed
+		t.time += (t.custom_delta * t.speed) - t.offset
 
 		return t.custom_delta
 	} else {
 		// Normal timer, use system timer.
 		t.delta = now - t.last_time
-		t.time = (now - t.start_time) * t.speed
+		t.time = ((now - t.start_time) * t.speed) - t.offset
 		t.last_time = now
 	}
 
