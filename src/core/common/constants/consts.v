f29477a8a467584fd2a4d4f74d29a50762be9049
xJunko <fireredz69420@gmail.com>
2024-02-23 19:08:49 +0800

    Kurarin: minor-5

--- src/core/common/constants/consts.v
+++ src/core/common/constants/consts.v
@@ -1,4 +1,4 @@
 module constants
 
 pub const game_name = 'Kurarin'
-pub const game_version = 'rewrite-0.0.6 [circles]'
+pub const game_version = 'rewrite-0.0.6.5 [osu!]'
--- src/v.mod
+++ src/v.mod
@@ -1,7 +1,7 @@
 Module {
 	name: 'dementia'
 	description: ''
-	version: '0.2.4'
+	version: '0.2.8'
 	license: ''
 	dependencies: []
 }
