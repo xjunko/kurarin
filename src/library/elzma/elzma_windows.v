700b089e83ea87f370b81ea6e67a12eecb88defa
xJunko <fireredz69420@gmail.com>
2024-02-24 09:07:54 +0900

    Thirdparty: WIP Windows fuckery

---
+++ src/library/elzma/elzma_windows.v
@@ -0,0 +1,5 @@
+module elzma
+
+pub fn decode_lzma(data []u8) !string {
+	return ''
+}
---
+++ src/library/tinyfiledialogs/tinyfiledialogs_windows.v
@@ -0,0 +1,6 @@
+module tinyfiledialogs
+
+// Public
+pub fn open_file_picker(title string, starting_path string, filters []string, filters_name string, multiple bool) string {
+	return ''
+}
